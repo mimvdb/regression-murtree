261f2126346def25074a900fec9591c8d39d505c
Mim van den Bos <mimvandenbos@gmail.com>
2023-07-22 21:17:38 +0200

    Generate train/test splits for all datasets

--- data/prepare.py
+++ data/prepare.py
@@ -3,39 +3,74 @@
 from pathlib import Path
 import json
 import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import KBinsDiscretizer
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
+
+
+def save_all_formats(dir, info, frame, filename):
+    osrt_base = dir / "osrt"
+    streed_pwc_base = dir / "streed_pwc"
+    streed_pwl_base = dir / "streed_pwl"
+
+    osrt_base.mkdir(parents=True, exist_ok=True)
+    streed_pwc_base.mkdir(exist_ok=True)
+    streed_pwl_base.mkdir(exist_ok=True)
+
+    X_cont = frame.loc[:,info["continuous_cols"]]
+    X_bin = frame.loc[:,info["binary_cols"]]
+    y = frame.iloc[:,0]
+
+    osrt_frame = pd.concat([X_bin, y], axis="columns")
+    streed_pwc_frame = pd.concat([y, X_bin], axis="columns")
+    streed_pwl_frame = pd.concat([y, X_cont, X_bin], axis="columns")
+
+    osrt_frame.to_csv(osrt_base / (filename + ".csv"), header=True, index=False)
+    streed_pwc_frame.to_csv(streed_pwc_base / (filename + ".csv"), sep=" ", header=False, index=False)
+    streed_pwl_frame.to_csv(streed_pwl_base / (filename + ".csv"), sep=" ", header=False, index=False)
+
+    with open(dir / (filename + ".json"), "w") as data_info:
+        json.dump({
+            "continuous_features": len(info["continuous_cols"]),
+            "mean_squared_error": mean_squared_error(y, np.full(len(y), np.mean(y))),
+            "instances": len(y)
+        }, data_info, indent=4)
+
 
 def prepare_all(bin_dir: Path, prep_dir: Path):
     with open(bin_dir / "info.json", "r") as info_json:
         infos = json.load(info_json)
 
-    osrt_base = prep_dir / "osrt"
-    osrt_base.mkdir(parents=True, exist_ok=True)
-
-    streed_pwc_base = prep_dir / "streed_pwc"
-    streed_pwc_base.mkdir(exist_ok=True)
-
-    streed_pwl_base = prep_dir / "streed_pwl"
-    streed_pwl_base.mkdir(exist_ok=True)
-
     for info in infos:
+        print(f"Preparing data: {info['name']}")
         frame = pd.read_csv(bin_dir / (info['filename'] + ".csv"))
+        save_all_formats(prep_dir, info, frame, info["filename"])
 
-        X_cont = frame.loc[:,info["continuous_cols"]]
-        X_bin = frame.loc[:,info["binary_cols"]]
-        y = frame.iloc[:,0]
+        # Make 5 train/test splits
+        disc = KBinsDiscretizer(n_bins=10, encode="onehot-dense", strategy="quantile", subsample=20000, random_state=42)
+        y_ = disc.fit_transform(frame.iloc[:, 0].values.reshape(-1, 1))
 
-        osrt_frame = pd.concat([X_bin, y], axis="columns")
-        streed_pwc_frame = pd.concat([y, X_bin], axis="columns")
-        streed_pwl_frame = pd.concat([y, X_cont, X_bin], axis="columns")
+        splits = []
+        for fold_index in range(5):
+            print(f"Fold: {fold_index}")
+            test_percentage = 0.2
+            train, test = train_test_split(frame, test_size=test_percentage, random_state = 42 + fold_index, stratify=y_)
+            train_file = f"{info['filename']}_train_{fold_index}"
+            test_file  = f"{info['filename']}_test_{fold_index}"
 
-        osrt_frame.to_csv(osrt_base / (info["filename"] + ".csv"), header=True, index=False)
-        streed_pwc_frame.to_csv(streed_pwc_base / (info["filename"] + ".csv"), sep=" ", header=False, index=False)
-        streed_pwl_frame.to_csv(streed_pwl_base / (info["filename"] + ".csv"), sep=" ", header=False, index=False)
+            save_all_formats(prep_dir, info, train, train_file)
+            save_all_formats(prep_dir, info, test, test_file)
+            splits.append({"train": train_file, "test": test_file})
 
-        with open(prep_dir / (info["filename"] + ".json"), "w") as data_info:
-            json.dump({}, data_info) # TODO write metadata from file, variance for R2, number of cont features for pwl etc.
+        info["splits"] = splits
+            
+
+    with open(prep_dir / "info.json", "w") as info_json:
+        json.dump(infos, info_json, indent=4)
+        
 
 
 if __name__ == "__main__":
