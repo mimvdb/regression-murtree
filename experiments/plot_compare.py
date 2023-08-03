82ad7ad3662a67aea223117ba878e6d4ad1b3c18
kjgm <kjgm_vd_linden@hotmail.com>
2023-08-03 16:33:25 +0200

    report means and wins

--- experiments/plot_compare.py
+++ experiments/plot_compare.py
@@ -5,9 +5,31 @@ from pathlib import Path
 SCRIPT_DIR = Path(__file__).parent.resolve()
 
 df = pd.read_csv(SCRIPT_DIR / "results.csv")
+df["dataset"] = df["train_data"].str.rsplit("_", n=1, expand=True)[0]
 
+print("\n * Mean Test R2 * \n")
 methods = df["method"].unique()
+means = df.groupby(["dataset", "method"])["test_r2"].mean()
+means = means.unstack('method')
+print(means)
 
+print("\n * Number of wins (folds): *\n")
+best_scores = df.groupby(["train_data"])["test_r2"].max()
+for i, m in enumerate(methods):
+    scores = df[df["method"] == m]
+    scores.index = scores["train_data"]
+    scores = scores["test_r2"]
+    wins = sum(scores >= best_scores)
+    print(f"{m}: {wins} wins")
+
+print("\n * Number of wins (mean per dataset): *\n")
+best_scores = means.max(axis=1)
+for i, m in enumerate(methods):
+    scores = means[m]
+    wins = sum(scores >= best_scores)
+    print(f"{m}: {wins} wins")
+
+print("\n * Wilcoxon comparison: *\n")
 for i, m1 in enumerate(methods):
     for j, m2 in enumerate(methods[i+1:]):
         
